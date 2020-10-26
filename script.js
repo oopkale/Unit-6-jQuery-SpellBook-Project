25f5509c704457474ab9ccce00e3609f11d5c62f
oopkale <60492520+oopkale@users.noreply.github.com>
2020-10-26 16:25:22 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("  .hide   ").click(function(){
+ $(".yourimage").toggle();
+});
+
+
+
+//Spell #2
+
+$(".slide ").click(function(){
+  $(".canada").slideToggle();
+});
+
+
+
+//Spell #3
+
+$("  .fade    ").click(function(){
+  $(".asia").fadeToggle();
+});
+
+
+
